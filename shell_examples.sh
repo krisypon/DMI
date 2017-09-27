3e1cb24a53e86591d0bbe70e2144439a9bcd1519
user <krisypon@inbox.lv>
2017-09-27 14:08:36 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#3. piemers
+N="Vards"
+echo $N
+unset N
+echo $N
+
+#2.piemers
+#N="Vards"
+#readonly N
+#echo $N
+#N="Vards Uzvards"
+#echo $N
+
+#1.piemers
+#N="Vards"
+#echo $N
+
+#0. piemers
+#history > history_20170927.txt
+
