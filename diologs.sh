d53ff6f7d48fe9b2889807f04dce9acf0e75819b
User <krisypon@inbox.lv>
2017-10-11 15:26:27 +0300

    20171011_15_30

---
+++ diologs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo -n "Ievadi x: "
+read x
+
+echo -n "Ievadi y: "
+read y
+
+sum=`expr $x + $y`
+echo "x+y="$sum
+st=`expr $x - $y`
+echo "x-y="$st
+reiz=`expr $x \* $y`
+echo "x*y="$reiz
+
+if [ $x -gt $y ]
+then
+echo "x lielaks par y"
+else 
+#if
+#[ $x -lt $y ]
+#then
+echo "y lielaks par x"
+#else
+#echo "x vienads ar y"
+fi
+
+if [ $x == $y ]
+then
+echo "x is equal to y"
+fi
+
+if [ $x != $y ]
+then
+echo "x is not equal to y"
+fi
--- shell_operators.sh
+++ shell_operators.sh
@@ -1,9 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 
-a=10
-b=20
+# 4.piemers - operacijas ar argumentiem
+if [ $# == 2 ]
+# ja (if) skriptam nodoto argumentu skaits ($#) ir vienads (==) ar $2
+then
+# tad tiek izpilditas visas sekojosas darbibas lidz...fi
+a=$1
+b=$2
+val41=`expr $a + $b`
+echo "$a + $b = "$val41
+val42=`expr $a - $b`
+echo "$a - $b = "$val42
+val43=`expr $a \* $b`
+echo "$a * $b = "$val43
+val44=`expr $a / $b`
+echo "$a / $b = "$val44
+val45=`expr $a % $b`
+echo "$a % $b = "$val45
+#...lidz sai vietai
+fi
+
+# 3.piemers - operacijas ar mainigajiem
+: <<'END'
+a=56
+b=32
+val31=`expr $a + $b`
+echo "$a + $b = "$val31
+val32=`expr $a - $b`
+echo "$a - $b = "$val32
+val33=`expr $a \* $b`
+echo "$a * $b = "$val33
+val34=`expr $a / $b`
+echo "$a / $b = "$val34
+val35=`expr $a % $b`
+echo "$a % $b = "$val35
+END
+
+# 2.piemers - operacijas ar konstantem
+: <<'END' #komentara bloka sakums
+val21=`expr 2 + 3`
+echo "2 + 3 = "$val21
+val22=`expr 2 - 3`
+echo "2 - 3 = "$val22
+val23=`expr 2 \* 3`
+echo "2 * 3 = "$val23
+val24=`expr 2 / 3`
+echo "2 / 3 = "$val24
+val25=`expr 2 % 3`
+echo "2 % 3 = "$val25
+END #komentaara bloka beigas
 
 # 1.piemers - izteiksmes pieraksts
+: <<'END' #komentara bloka sakums
 val11="expr 2+2"
 echo "2 + 2 rezultats (pedinas, nav atstarpes) "$val11
 val12="expr 2 + 2"
@@ -14,18 +62,7 @@ val14='expr 2 + 2'
 echo "2 + 2 rezultats (parastie apostrofi, ir atstarpes) "$val14
 val15=`expr 2+2`
 echo "2 + 2 rezultats (neparastie apostrofi, nav atstarpes) "$val15
-val16='expr 2 + 2'
+val16=`expr 2 + 2`
 echo "2 + 2 rezultats (neparastie apostrofi, ir atstarpes) "$val16
-
-#val=`expr 2 + 2`
-#echo "Total value : $val"
-
-#val=`expr $a + $b`
-#echo "Total value : $val"
-
-#val=`expr $a - $b`
-#echo "Total value : $val"
-
-#val=`expr $a \* $b2`
-#echo "Total value : $val"
-
+END
+#komentara bloka beigas
