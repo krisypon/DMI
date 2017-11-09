377bcc5f99679a090057ce718c00a2341268fde5
User <krisypon@inbox.lv>
2017-11-09 14:44:32 +0200

    20171109

---
+++ PYTHON/range.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+a = range(5)
+print "a = range(5)"
+print (a)
+print " "
+
+b = range(1, 8)
+print "b = range(1, 8)"
+print (b)
+print " "
+
+c = range(0, 14, 3)
+print "c = range(0, 14, 3)"
+print (c)
+print " "
+
+d = range(8, 1, -1)
+print "d = range(8, 1, -1)"
+print (d)
