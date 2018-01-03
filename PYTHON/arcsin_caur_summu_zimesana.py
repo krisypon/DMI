95c0a9db7bdeb8070275006dd49c35c91c48b62e
User <krisypon@inbox.lv>
2018-01-04 00:05:56 +0200

    20180103

--- PYTHON/arcsin_caur_summu_zimesana.py
+++ PYTHON/arcsin_caur_summu_zimesana.py
@@ -1,21 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-1.0, 1.0, 0.01)
+x = np.arange(-1, 1, 0.01)
 
-y = np.arcsin(x)
-plt.plot(x, y)
+#y = np.arcsin(x)
+#plt.plot(x, y)
 
 def my_asin(x,n):
     k = 0
     a = x
     S = a
-    while k < 500:
+    while k < n:
         k += 1
-        a = a*((2*k-1)**2*2*k*x*x)/(k**2*4*(2*k+1))
+        a = a*((2*k-1)*(2*k-1)*x*x)/(k*2*(2*k+1))
         S += a
     return S
 
-yy = my_asin(x,10)
-plt.plot(x, yy+0.5, 'r')
+yy = my_asin(x,500)
+plt.plot(x, yy, 'r')
 plt.show()
